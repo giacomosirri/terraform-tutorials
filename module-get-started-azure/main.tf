1a85a74504f9c94a581767d56ca2cbf23160b9eb
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-02-24 20:41:26 +0100

    Remove useless variables

--- module-get-started-azure/main.tf
+++ module-get-started-azure/main.tf
@@ -13,9 +13,7 @@ terraform {
 provider "azurerm" {
   features {
   }
-  tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
-  client_id       = var.client_id
   use_oidc        = true
 }
 
