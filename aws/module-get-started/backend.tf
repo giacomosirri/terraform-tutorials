51e9e1ee62ea9688afb0b050e92dce137d81dbd4
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-03-03 17:36:24 +0100

    Test

--- aws/module-get-started/backend.tf
+++ aws/module-get-started/backend.tf
@@ -3,5 +3,6 @@ terraform {
     storage_account_name = "terraformstorageacc1234"
     container_name       = "terraformstatefilescontainer"
     key                  = "aws.terraform.tfstate"
+    resource_group_name  = "terraform-rg"
   }
 }
