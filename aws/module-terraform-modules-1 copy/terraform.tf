dc8f9404766550accacd8e8d0fae0f45c7d80bfe
Giacomo Sirri <sirrigiacomo9@gmail.com>
2025-03-07 11:47:22 +0100

    Add new terraform module

---
+++ aws/module-terraform-modules-1 copy/terraform.tf
@@ -0,0 +1,12 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.49.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
